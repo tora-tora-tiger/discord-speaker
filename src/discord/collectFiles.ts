cdc731fe6d70f7cf64a4a22d751fdd9d8c23ab45
tora <bmvdsfy@gamil.com>
2025-04-01 01:23:00 +0900

    ディレクトリ内のファイル集めるのをモジュール化

---
+++ src/discord/collectFiles.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
+export default async function collectFiles (
+  directory: string,
+  fileType: string,
+  callback: (filePath: string) => Promise<void> | void,
+): Promise<void> {
+
+  const folderPath = path.join(__dirname, directory);
+  const files = fs.readdirSync(folderPath).filter(
+    (file) => file.endsWith(fileType),
+  );
+
+  console.log("found", files);
+
+  for (const file of files) {
+    const filePath = path.join(folderPath, file);
+    await callback(filePath);
+  }
+}
--- src/discord/index.ts
+++ src/discord/index.ts
@@ -1,8 +1,9 @@
-import { Client, Events, GatewayIntentBits, Collection } from 'discord.js';
+import { Client, Events, GatewayIntentBits, Collection, Interaction } from 'discord.js';
 import { token } from '../../config.json';
-import path from 'path';
+// import path from 'path';
 import { pathToFileURL } from 'url';
-import fs from 'fs';
+// import fs from 'fs';
+import collectFiles from './collectFiles';
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
@@ -11,45 +12,64 @@ const client = new Client({
 // スラッシュコマンドの登録
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, "commands");
-const commandFolders = fs.readdirSync(foldersPath);
+// const foldersPath = path.join(__dirname, "commands");
+// const commandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-  console.log(`Loading commands from ${commandsPath}`);
-	const commandFiles = fs
-    .readdirSync(commandsPath)
-		.filter((file) => file.endsWith(".ts"));
+// for (const folder of commandFolders) {
+// 	const commandsPath = path.join(foldersPath, folder);
+//   console.log(`Loading commands from ${commandsPath}`);
+// 	const commandFiles = fs
+//     .readdirSync(commandsPath)
+// 		.filter((file) => file.endsWith(".ts"));
   
-  console.log(commandFiles);
+//   console.log(commandFiles);
     
-  for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    (async () => {
-      console.log("koko", filePath);
-      const command = await import(pathToFileURL(filePath).href);
-      // const command = require(filePath);
-      console.log("import");
+//   for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//     (async () => {
+//       const command = await import(pathToFileURL(filePath).href);
 
-      if (command.data && command.execute) {
-        console.log("set command: ", command.data.name);
-        client.commands.set(command.data.name, command);
-      } else {
-        console.log(
-          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
-        );
-      }
-    })();
-	}
-}
+//       if (command.data && command.execute) {
+//         console.log("set command: ", command.data.name);
+//         client.commands.set(command.data.name, command);
+//       } else {
+//         console.log(
+//           `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+//         );
+//       }
+//     })();
+// 	}
+// }
+
+collectFiles(
+  "commands",
+  ".ts",
+  async (filePath) => {
+    const command = await import(pathToFileURL(filePath).href);
+
+    if (command.data && command.execute) {
+      console.log("set command: ", command.data.name);
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+      );
+    }
+  }
+)
 
 client.once(Events.ClientReady, (readyClient) => {
   console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
-client.on(Events.InteractionCreate, async (interaction) => {
+client.on(Events.InteractionCreate, async (interaction: Interaction) => {
   if (!interaction.isChatInputCommand()) return;
-  const command = interaction.client.commands.get(interaction.commandName);
+  console.log(client.commands.keys());
+  const command = interaction.client.commands.get(interaction.commandName)
+  console.log(command);
+
+  console.log("interaction: ", interaction.commandName);
+  console.log("command: ", command);
 
   if (!command) {
     console.error(
@@ -59,6 +79,7 @@ client.on(Events.InteractionCreate, async (interaction) => {
   }
   try {
     await command.execute(interaction);
+    console.log("executed command: ", interaction.commandName);
   } catch (error) {
     console.error(`Error executing ${interaction.commandName}`);
     console.error(error);
