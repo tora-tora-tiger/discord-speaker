0f38754ccaf4ecc1952edebd874c26cf2efd8adb
tora <bmvdsfy@gmail.com>
2025-04-07 17:00:17 +0900

    dev削除

--- src/discord/commands/utility/test.ts
+++ src/discord/commands/utility/test.ts
@@ -1,34 +1,13 @@
 import { joinVoiceChannel } from '@discordjs/voice';
-import {SlashCommandBuilder, CommandInteraction, ChannelType} from 'discord.js';
+import {SlashCommandBuilder, CommandInteraction, ChannelType, Options} from 'discord.js';
+import asyncFind from '@/utility/asyncFind';
 
 const data = new SlashCommandBuilder()
   .setName('test')
   .setDescription('test verious commands');
 
 const execute = async function(interaction: CommandInteraction) {
-  // チャンネル一覧を取得して，そこからコマンドを打った人が入っているVCを探す
-  const guild = interaction.guild;
-  const guildChannelManager = guild?.channels;
-  if(!guildChannelManager) return;
-  const channels = guildChannelManager.cache;
-  const voiceChannels = channels.filter(channel => channel.type === ChannelType.GuildVoice);
-  console.log("found channels", voiceChannels);
-  const vcChannel = voiceChannels.find(channel => {
-    const members = channel.members;
-    return members.has(interaction.user.id);
-  });
-
-  if(!vcChannel) {
-    await interaction.reply("You are not in a voice channel.");
-    return;
-  }
-
-  const connection = joinVoiceChannel({
-    channelId: vcChannel.id,
-    guildId: guild.id,
-    adapterCreator: guild.voiceAdapterCreator,
-  });
-  await interaction.reply(`Joined ${vcChannel.name}`);
+  await interaction.reply("null");
 }
 
 export default { 
