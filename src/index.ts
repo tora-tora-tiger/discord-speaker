66f36f907c1b3f70af2c1142b3e25f98bef3ac3a
tora <bmvdsfy@gmail.com>
2025-04-11 00:21:11 +0900

    エラーにスコープ

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-import Talk from "@/Talk";
 import Discord from "@/discord";
-
-const talk = new Talk();
-
-talk.setHost("0.0.0.0");
-talk.setPort(50021);
 
 const discord = new Discord();
 discord.start()
 
 process.on("SIGINT", async () => {
-  console.log("SIGINT received. Exiting...");
+  console.log("[main] SIGINT received. Exiting...");
   await discord.close();
   process.exit(0);
 });
